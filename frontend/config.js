39abac86e86ae5288b99c5763a3aaec01eb5c0a1
EugeneBruno <ayalogueugene3@gmail.com>
2025-06-04 10:12:31 +0100

    Updated the backend server

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,1 @@
-export  const  BASE_URL = window.location.hostname === 'localhost'
-    ? 'http://localhost:8000'
-    :  'http://192.168.0.185:8000'
+export  const  BASE_URL = 'https://swiftcart-production.up.railway.app'
